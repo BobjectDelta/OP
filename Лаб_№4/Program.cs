95787ab982cd05b4fadcc7a69f0c3763f4e05ee1
BobjectDelta <90173834+BobjectDelta@users.noreply.github.com>
2021-11-01 11:02:28 +0300

    Add files via upload

---
+++ Лаб_№4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v = 0;
+            double v1 = 1.5;
+            double v2 = 0;
+            double v3 = 0;
+            Int32 n = Convert.ToInt32(Console.ReadLine());
+            for (double i = 4; i <= n; i++)
+            {
+                v = (i + 1)/(i * i + 1) * v1 - v2 * v3;
+                v3 = v2;
+                v2 = v1;
+                v1 = v;
+            }
+            Console.WriteLine(v);
+        }   
+        
+        
+    }
+}
